1c3fa1495d98d215c54ea4495dbf9990cde0e709
Vitaly Rubanov <111revun@mail.ru>
2018-12-14 14:49:54 +0300

    заполнены Manager, Desigener

--- src/main/java/itCompany/employee/Desigener.java
+++ src/main/java/itCompany/employee/Desigener.java
@@ -1,4 +1,53 @@
 package itCompany.employee;
 
-public class Desigener {
+public class Desigener extends Employee {
+    private String[] desigenerSkillSet;
+    private String   computer;
+
+    public Desigener(){
+
+    }
+
+    public Desigener(String[] desigenerSkillSet, String computer) {
+        this.desigenerSkillSet = desigenerSkillSet;
+        this.computer = computer;
+    }
+
+    public Desigener(String firstName, String lastName, int age, int salary, String[] desigenerSkillSet, String computer) {
+        super(firstName, lastName, age, salary);
+        this.desigenerSkillSet = desigenerSkillSet;
+        this.computer = computer;
+    }
+
+    public String[] getDesigenerSkillSet() {
+        return desigenerSkillSet;
+    }
+
+    public void setDesigenerSkillSet(String[] desigenerSkillSet) {
+        this.desigenerSkillSet = desigenerSkillSet;
+    }
+
+    public String getComputer() {
+        return computer;
+    }
+
+    public void setComputer(String computer) {
+        this.computer = computer;
+    }
+
+    @Override
+    public void work() {
+        draw();
+        showSketches();
+        draw();
+
+    }
+
+    private void draw(){
+        System.out.println("Рисую рисование...");
+    }
+
+    public void showSketches(){
+        System.out.println("Есть чем поглядеть?..");
+    }
 }
--- src/main/java/itCompany/employee/Manager.java
+++ src/main/java/itCompany/employee/Manager.java
@@ -1,4 +1,62 @@
 package itCompany.employee;
 
-public class Manager {
+public class Manager extends Employee {
+
+    private boolean researchingMarket;
+    private boolean teamManaging;
+
+    public Manager() {
+    }
+
+    public Manager(String firstName, String lastName, int age, int salary) {
+        super(firstName, lastName, age, salary);
+    }
+
+    public Manager(boolean researchingMarket, boolean teamManaging) {
+        this.researchingMarket = researchingMarket;
+        this.teamManaging = teamManaging;
+    }
+
+    public Manager(String firstName, String lastName, int age, int salary, boolean researchingMarket, boolean teamManaging) {
+        super(firstName, lastName, age, salary);
+        this.researchingMarket = researchingMarket;
+        this.teamManaging = teamManaging;
+    }
+
+    private void manage(){
+        if (researchingMarket){
+            System.out.println("исследую рынок");
+        }
+        if (teamManaging){
+            System.out.println("Настоящие герои всегда идут в обход!..");
+        }
+
+    }
+
+    public void lounchMeeting() {
+        System.out.println("Всем стоять, бояться!");
+    }
+
+
+
+    @Override
+    public void work() {
+
+    }
+
+    public boolean isResearchingMarket() {
+        return researchingMarket;
+    }
+
+    public void setResearchingMarket(boolean researchingMarket) {
+        this.researchingMarket = researchingMarket;
+    }
+
+    public boolean isTeamManaging() {
+        return teamManaging;
+    }
+
+    public void setTeamManaging(boolean teamManaging) {
+        this.teamManaging = teamManaging;
+    }
 }
