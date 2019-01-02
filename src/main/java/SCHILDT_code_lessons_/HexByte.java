6567813e7e76e10bf9ab6449ec615db74f0fd799
Vitaly Rubanov <111revun@mail.ru>
2019-01-02 15:05:46 +0300

    Глава 4 операции, подразрядные операции: Маскирвоание двоичных разрядов расширения знака

---
+++ src/main/java/SCHILDT_code_lessons_/HexByte.java
@@ -0,0 +1,15 @@
+package SCHILDT_code_lessons_;
+
+//Маскирвоание двоичных разрядов расширения знака
+
+public class HexByte {
+    public static void main (String args[]){
+        char hex[] = {
+                '0', '1', '2', '3', '4', '5', '6', '7',
+                '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
+        };
+        byte b = (byte) 0xf1;
+        System.out.println("b = 0x" + hex[(b >> 4) & 0x0f]
+                + hex [b & 0x0f]);
+    }
+}
