7f1aa4315a03670f7643b08c1986ab62b4d469dd
Vitaly Rubanov <111revun@mail.ru>
2019-01-02 21:10:20 +0300

    Глава 4 операции, операции: Продемонстрировать тернарную операцию "?"

---
+++ src/main/java/SCHILDT_code_lessons_/Ternary.java
@@ -0,0 +1,17 @@
+package SCHILDT_code_lessons_;
+
+//Продемонстрировать тернарную операцию "?"
+public class Ternary {
+    public static void main(String args[]) {
+        int i, k;
+        i = 10;
+        k = i < 0 ? -i : i; // получить абсолютное значение i
+        System.out.print("Абсолютное значение ");
+        System.out.println(i + " равно " + k);
+
+        i = -10;
+        k = i < 0 ? -i : i; //получить абсолютное значение i
+        System.out.print("Абсолютное значение ");
+        System.out.println(i + " равно " + k);
+    }
+}
