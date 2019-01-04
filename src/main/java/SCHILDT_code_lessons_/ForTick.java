c35aa006f96c8aaa3ee3da54cf51690744b96915
Vitaly Rubanov <111revun@mail.ru>
2019-01-04 14:21:04 +0300

    Глава 5 управляющие операторы. Операторы цикла. for. Продемонстрировать применение оператора for.

---
+++ src/main/java/SCHILDT_code_lessons_/ForTick.java
@@ -0,0 +1,12 @@
+package SCHILDT_code_lessons_;
+
+//Продемонстрировать применение оператора for
+
+public class ForTick {
+    public static void main(String[] args) {
+        int n;
+
+        for (n = 10; n > 0; n--)
+            System.out.println("Такт " +  n);
+    }
+}
