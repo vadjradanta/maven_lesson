ad45f6828010ad1eb4bf4a605434cb4ae8f29953
Vitaly Rubanov <111revun@mail.ru>
2019-01-04 16:44:50 +0300

    Глава 5. Управляющие операторы. for. Отдельные части оператора for могут отсутствовать.

---
+++ src/main/java/SCHILDT_code_lessons_/ForVar.java
@@ -0,0 +1,17 @@
+package SCHILDT_code_lessons_;
+
+// Отдельные части оператора for могут отсутствовать
+
+public class ForVar {
+    public static void main(String[] args) {
+        int i;
+        boolean done = false;
+
+        i = 0;
+        for ( ; !done; ){
+            System.out.println("i равно " + i);
+            if (i == 10) done = true;
+            i++;
+        }
+    }
+}
