f4ae17f7710a4f0c96db2a649c213777c3778ab4
Vitaly Rubanov <111revun@mail.ru>
2019-01-04 16:30:49 +0300

    Глава 5 управляющие операторы. Использование запятой. Рациональная организация цикла for с использованием запятых.

---
+++ src/main/java/SCHILDT_code_lessons_/Comma.java
@@ -0,0 +1,14 @@
+package SCHILDT_code_lessons_;
+
+// Рациональная организация цикла for с использованием запятых
+
+public class Comma {
+    public static void main(String[] args) {
+        int a, b;
+
+        for (a = 1, b = 4; a < b; a++, b--){
+            System.out.println("a = " + a);
+            System.out.println("b = " + b);
+        }
+    }
+}
