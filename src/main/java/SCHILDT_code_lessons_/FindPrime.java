03cc6d4de121e9804ad9e7e975ea14902b764155
Vitaly Rubanov <111revun@mail.ru>
2019-01-04 14:33:57 +0300

    Глава 5 управляющие операторы. Операторы цикла. for. Проверить на простые числа.

---
+++ src/main/java/SCHILDT_code_lessons_/FindPrime.java
@@ -0,0 +1,24 @@
+package SCHILDT_code_lessons_;
+
+//Проверить на простые числа
+
+public class FindPrime {
+    public static void main (String[] args) {
+        int num;
+        boolean isPrime;
+
+        num = 1;
+
+        if (num < 2) isPrime = false;
+        else isPrime = true;
+
+        for (int i = 2; i <= num/i; i++) {
+            if ((num % i) == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) System.out.println("Простое число");
+        else System.out.println("Не простое число");
+    }
+}
