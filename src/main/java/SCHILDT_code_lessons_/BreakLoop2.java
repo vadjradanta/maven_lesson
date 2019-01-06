74778c90e0de96bff5d62f867c220a613b6320e6
Vitaly Rubanov <111revun@mail.ru>
2019-01-06 16:38:34 +0300

    Глава 5. Управляющие операторы. Операторы прерывания. Break. Применение оператора break для выхода из цикла while

---
+++ src/main/java/SCHILDT_code_lessons_/BreakLoop2.java
@@ -0,0 +1,18 @@
+package SCHILDT_code_lessons_;
+
+//Применение оператора break для выхода из цикла while
+
+public class BreakLoop2 {
+    public static void main (String[] args) {
+        int i = 0;
+
+        while (i < 100) {
+            if (i == 10) break; //выход из цикла, если значение
+                                //переменной i равно 10
+            System.out.println("i: " + i);
+            i++;
+        }
+        System.out.println("Цикл завершён!");
+    }
+
+}
