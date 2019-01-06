15498991c94df59cc41bd464617118d91059c574
Vitaly Rubanov <111revun@mail.ru>
2019-01-06 19:14:36 +0300

    Глава 5. Управляющие операторы. Операторы прерывания. return. Продемонстрировать применение оператора return.

---
+++ src/main/java/SCHILDT_code_lessons_/Return.java
@@ -0,0 +1,16 @@
+package SCHILDT_code_lessons_;
+
+//Продемонстрировать применение оператора return
+
+public class Return {
+    public static void main(String[] args) {
+        boolean t = true;
+
+        System.out.println("До возврата.");
+
+        if (t) return; //возврат в вызывающий код
+
+        System.out.println("Этот оператор выполнятся не будет");
+
+    }
+}
